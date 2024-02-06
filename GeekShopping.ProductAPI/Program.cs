62a5ae23a74d78dcaa846857775045ea5eb38946
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2024-02-06 17:22:42 -0300

    feat: add db context

--- GeekShopping.ProductAPI/Program.cs
+++ GeekShopping.ProductAPI/Program.cs
@@ -1,11 +1,19 @@
+using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<MySQLContext>(options => options.
+UseMySql(connection, new MySqlServerVersion(new Version(8, 0, 5))));
 
 var app = builder.Build();
 
