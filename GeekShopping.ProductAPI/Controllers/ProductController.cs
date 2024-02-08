a07656354811aa2619a2e8f57a2c589c0dbf7283
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2024-02-08 18:20:27 -0300

    feat: add controller

---
+++ GeekShopping.ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,59 @@
+﻿using GeekShopping.ProductAPI.Data.ValueObjects;
+using GeekShopping.ProductAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.ProductAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private IProductRepository _repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet]  
+        public async Task<IActionResult> FindAll()
+        {
+            IEnumerable<ProductVO> products = await _repository.FindAll();
+            if(products == null) return NotFound();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> FindById(long id)
+        {
+            ProductVO product = await _repository.FindById(id);
+            if (product == null) return NotFound();
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductVO vo)
+        {
+            if (vo == null) return BadRequest();
+            ProductVO product = await _repository.Create(vo);
+            return Ok(product);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(ProductVO vo)
+        {
+            if(vo == null) return BadRequest();
+            ProductVO product = await _repository.Update(vo);
+            return Ok(product);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(long id)
+        {
+            bool deleted = await _repository.Delete(id);
+            if (!deleted) return BadRequest();
+            return Ok(deleted);
+        }
+
+    }
+}
--- GeekShopping.ProductAPI/Data/ValueObjects/ProductVO.cs
+++ GeekShopping.ProductAPI/Data/ValueObjects/ProductVO.cs
@@ -5,6 +5,7 @@ namespace GeekShopping.ProductAPI.Data.ValueObjects
 {
     public class ProductVO
     {
+        public long Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
         public string Description { get; set; }
