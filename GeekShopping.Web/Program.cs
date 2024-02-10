6ff721c8414b20a2f75ac781b24225aeb73816a9
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2024-02-09 23:22:04 -0300

    feat: add HttpClient and frontend ProductService

--- GeekShopping.Web/Program.cs
+++ GeekShopping.Web/Program.cs
@@ -1,7 +1,12 @@
+using GeekShopping.Web.Services;
+using GeekShopping.Web.Services.IService;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient<IProductService, 
+    ProductService>(c => c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:ProductAPI"]));
 
 var app = builder.Build();
 
